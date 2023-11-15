6579a57175b3244e1127f25d5607c951f7c16556
Tocopro <nicwac200g@mail.com>
2023-11-14 20:33:50 -0600

    added files build

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    dbName: "book-search-db",
+  }
+);
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -41,4 +41,4 @@ const startApolloServer = async () => {
 };
 
 // Call the async function to start the server
-startApolloServer();
+startApolloServer(typeDefs, resolvers);
